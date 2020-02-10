9076919aad999e1d691baba40126dd2a078cdf04
OmkarK45 <45557594+OmkarK45@users.noreply.github.com>
2020-02-10 19:56:21 +0530

    Recursive addition

---
+++ codechef2.py
@@ -0,0 +1,10 @@
+cases = 0
+# def add():
+#     x,y =  map(int, input().split())
+#     return x+y
+cases = int(input())
+
+for i in range(0,cases):
+    x,y =  map(int, input().split())
+    ans = x+y
+    print(ans)
